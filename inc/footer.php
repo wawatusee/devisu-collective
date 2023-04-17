14ce9a6f068b2ca67956b0197ff2f6f2387a48a7
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-04-17 14:41:02 +0200

    menu, footer, style, main.css Esthétique

--- inc/footer.php
+++ inc/footer.php
@@ -1,13 +1,13 @@
 <footer>
     <section id="sitemap">
-        <h2>Navigation</h2>
+        
             <div class="footerNav">
                 <nav class="navfooterbloc">
-                    <h3>Menu principal</h3>
+                    <h2>Menu principal</h2>
                     <?php echo $menuMain_view?>
                 </nav>
                 <!--<nav class="navfooterbloc">
-                    <h3>Ils en parlent</h3>
+                    <h2>Ils en parlent</h2>
                     <a href="https://youtu.be/h3RULNKo_wo" target="_blank" rel="noopener noreferrer">TASTED by Andreas Larsson</a>
                     <a href="https://www.hachette-vins.com/guide-vins/producteurs/47281/scea-behaghel-ch-peyrines/" target="_blank" rel="noopener noreferrer">Hachette vins</a>
                     <a href="https://www.concourslyon.com/wine/33873/chateau-peyrines" target="_blank" rel="noopener noreferrer">Concours international de lyon</a>
--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,4 @@
 <header>
 <img class="logo" src="img/deco/main-logo.png" alt="logo-site">
-<h1><?php echo $titleWebSite[0]?><?php echo " ".$titleWebSite[1]?><span><?php echo $titleWebSite[2]?></span></h1>
+<h1 class="maintitle"><span><?php echo $titleWebSite[0]?></span><span><?php echo " ".$titleWebSite[1]?></span></h1>
 </header>
