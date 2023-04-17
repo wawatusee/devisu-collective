d8458d4a43be598cf53012068a618bf7fa7a3fb7
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-04-17 09:32:57 +0200

    Player en modale fonctionnel mais bon

--- inc/footer.php
+++ inc/footer.php
@@ -6,20 +6,13 @@
                     <h3>Menu principal</h3>
                     <?php echo $menuMain_view?>
                 </nav>
-                <nav class="navfooterbloc">
+                <!--<nav class="navfooterbloc">
                     <h3>Ils en parlent</h3>
                     <a href="https://youtu.be/h3RULNKo_wo" target="_blank" rel="noopener noreferrer">TASTED by Andreas Larsson</a>
                     <a href="https://www.hachette-vins.com/guide-vins/producteurs/47281/scea-behaghel-ch-peyrines/" target="_blank" rel="noopener noreferrer">Hachette vins</a>
                     <a href="https://www.concourslyon.com/wine/33873/chateau-peyrines" target="_blank" rel="noopener noreferrer">Concours international de lyon</a>
                     <a href="https://agriculture.gouv.fr/la-haute-valeur-environnementale-une-mention-valorisante-pour-les-agriculteurs-et-leurs-pratiques" target="_blank" rel="noopener noreferrer">Haute valeur environnementale</a>
-                </nav>
+                </nav>-->
             </div>
     </section>
-    <nav id="menuRS" class="nav-rs">
-        <?php 
-                    foreach($menuRS as $item){
-                        echo "<a href=".$item->page." title='".$item->titre."' target='_blank'><div class='rs ".$item->titre."'></div></a>";
-                    }
-        ?>
-    </nav>
 </footer>
--- inc/pages/productions.php
+++ inc/pages/productions.php
@@ -1,24 +1,19 @@
 <section id="portfolio">
-    <a href="#id01">Ouvrir la fenêtre modale</a>
         <h2>PORTFOLIO</h2>
         <div id="id01" class="modal">
             <div class="modal-dialog">
                 <div class="modal-content">
-                <header class="container"> 
-                    <a href="#" class="closebtn">×</a>
-                </header>
-                <div class="container">
-                <video id="playervideo" controls width="640" height="480" poster="img/content/thumbs-videos/mariamateaser.jpg" autoplay muted>
-                     <source src="videos/MARIAMA-TEASER-FINAL-FINAL-1.2.mp4" type="video/mp4">
-                </video>
+                    <div class="container"> 
+                        <a href="#" class="closebtn">×</a>
                     </div>
-
+                    <div class="container">
+                        <video id="playervideo" controls width="640" height="480" poster="img/content/thumbs-videos/mariamateaser.jpg" autoplay muted>
+                            <source src="videos/MARIAMA-TEASER-FINAL-FINAL-1.2.mp4" type="video/mp4">
+                        </video>
+                    </div>
                 </div>
             </div>
-        </div> 
-            <video id="playervideo" controls width="640" height="480" poster="img/content/thumbs-videos/mariamateaser.jpg" autoplay muted>
-                <source src="videos/MARIAMA-TEASER-FINAL-FINAL-1.2.mp4" type="video/mp4">
-            </video>
+        </div>
             <?php require_once("../src/model/portfolio_model.php");
             $portfolioData=new PortfolioModel("../json/portfolio.json");
             $contentPortfolio=$portfolioData->getPortfolio();
