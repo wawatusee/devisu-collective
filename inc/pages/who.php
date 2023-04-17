7e2e28fc86d077cba5d03bf45bf2db70f0826d61
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-04-17 17:43:11 +0200

    services.php, nettoyage de toutes les balises et css inutiles

--- inc/pages/who.php
+++ inc/pages/who.php
@@ -1,9 +1,9 @@
-<section class="who">
-<h2>WHO WE ARE</h2>    
-<p>We are DEVISU. We are a collective of creative companies and freelancers who specialize in producing audiovisual campaigns for both non-profit and for-profit organizations. Our team is made up of talented professionals with diverse backgrounds, skills, and experience in various areas of the audiovisual industry.</p> 
-<p>For the last 5 years we have specialized in creating communication campaigns dedicated to NGOs. Our goal is to Amplifying voices to igniting change!</p> 
-<p>We are proud to have worked with a wide range of organizations such as the European Commission, the European peace building liaison offices (EPLO), the United Nation, the kings baudouin foundation …</p> 
-<p>We are always looking for new and exciting opportunities to help organizations tell their stories through the power of visual media.</p>
-<p>At DEVISU, we believe that collaboration and diversity are key to producing exceptional work. By working together, we bring a wealth of perspectives and ideas to every project we undertake, ensuring that our clients receive the highest quality content that meets their unique needs.</p>
-<p>If you're looking for a team of dedicated and creative professionals to help you bring your vision to life, look no further than DEVISU. We are here to help you communicate your message, connect with your audience, and make a meaningful impact in the world.</p>
+<section class="who only-chapter">
+    <h2>WHO WE ARE</h2>    
+    <p>We are DEVISU. We are a collective of creative companies and freelancers who specialize in producing audiovisual campaigns for both non-profit and for-profit organizations. Our team is made up of talented professionals with diverse backgrounds, skills, and experience in various areas of the audiovisual industry.</p> 
+    <p>For the last 5 years we have specialized in creating communication campaigns dedicated to NGOs. Our goal is to Amplifying voices to igniting change!</p> 
+    <p>We are proud to have worked with a wide range of organizations such as the European Commission, the European peace building liaison offices (EPLO), the United Nation, the kings baudouin foundation …</p> 
+    <p>We are always looking for new and exciting opportunities to help organizations tell their stories through the power of visual media.</p>
+    <p>At DEVISU, we believe that collaboration and diversity are key to producing exceptional work. By working together, we bring a wealth of perspectives and ideas to every project we undertake, ensuring that our clients receive the highest quality content that meets their unique needs.</p>
+    <p>If you're looking for a team of dedicated and creative professionals to help you bring your vision to life, look no further than DEVISU. We are here to help you communicate your message, connect with your audience, and make a meaningful impact in the world.</p>
 </section>
