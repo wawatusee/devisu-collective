733c51172bdce82a239391b5c9539ae21ae61dd4
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-04-27 12:15:32 +0200

    vavview, ajout de guillemets autour des attributs de liens

--- src/view/view_menus.php
+++ src/view/view_menus.php
@@ -12,7 +12,7 @@ class ViewMenu{
         foreach($menuArray as $item){
             if($singlePage){
                 $this->viewMenu.="<a href=#".$item->page.">".$item->titre."</a>";
-            }else $this->viewMenu.="<a href="."?page=".$item->page.">".$item->titre."</a>";
+            }else $this->viewMenu.="<a href='"."?page=".$item->page."'>".$item->titre."</a>";
             
         }
         $viewMenu=$this->viewMenu;
